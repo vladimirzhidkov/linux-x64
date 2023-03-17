dd80b71df397df62c84ceb8f9fc09ddc4eb600e4
vlad zhidkov <vladimir.zhidkov@icloud.com>
2023-03-17 19:14:36 +0000

    minor changes

--- hello/main.s
+++ hello/main.s
@@ -10,7 +10,7 @@ _start:
         mov     $1,             %rdi        # file descriptor (stdout = 1)
         mov     $msg,           %rsi        # string address
         mov     $msg_length,    %rdx        # string length
-        call _write        
+        call    _write        
 
         # exit(0)
         mov     $0,     %rdi        # exit return value
